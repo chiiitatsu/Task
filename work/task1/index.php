9264b1156809a910fb675f151bda214ceecc1c97
Tatsuya Wachi <wachi@MacBook-Pro.local>
2020-09-24 00:39:32 +0900

    課題1

--- work/task1/index.php
+++ work/task1/index.php
@@ -8,17 +8,14 @@
 </head>
 <body>
     <h1>FIzzBuzz問題</h1>
-    <form class="num" action="submit.php" method="get">
+
+    <form class="fizzBuzz" action="index.php" method="post">
         <dl>
             <dt>FizzNum:</dt>
-            <?php $fizzNum = $_GET["FizzNum"]; ?>
-            <dd><input type="text" size="22" name="FizzNum" 
-            value="<?php echo $fizzNum ?>" placeholder="整数値を入力してください"></dd>
-
+            <dd><input type="text" size="22" name="FizzNum" placeholder="整数値を入力してください"></dd>
+            
             <dt>BuzzNum:</dt>
-            <?php $buzzNum = $_GET["BuzzNum"]; ?>
-            <dd><input type="text" size="22" name="BuzzNum" 
-            value= "<?php echo $buzzNum ?>" placeholder="整数値を入力してください"></dd>
+            <dd><input type="text" size="22" name="BuzzNum" placeholder="整数値を入力してください"></dt>
         </dl>
     
         <input type="submit" id="botton" value="実行">
@@ -28,48 +25,56 @@
     <p><?php echo '【出力】'; ?></p>
 
     <p>
+        
         <?php
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+            $fizzNum = $_POST["FizzNum"];
+            $buzzNum = $_POST["BuzzNum"]; 
+
             //倍数計算のための変数
             $num1 = $fizzNum;
             $num2 = $buzzNum;
-       
+            
+            //空欄・文字列だった場合
+            if($fizzNum === false || $buzzNum === false){
+                echo '整数値を入力してください';
+                exit;
+            }
+            
+            //小数・マイナスだった場合
+            if(ctype_digit(strval($fizzNum)) === false || ctype_digit(strval($buzzNum)) === false){
+                echo '整数値を入力してください';
+                exit;
+            }
+
             for($i = 1; $i < 100 ; $i++){
 
-                //空欄・文字列だった場合
-                if($fizzNum == 0 || $buzzNum == 0){
-                  echo '整数値を入力してください';
-                  break;
-                }
-                //小数・マイナスだった場合
-                if(ctype_digit(strval($fizzNum)) == 0 || ctype_digit(strval($buzzNum)) == 0){
-                    echo '整数値を入力してください';
-                    break;
-                }
-
                 //BuzzFizz表示
-                if($i == $fizzNum && $i == $buzzNum){
+                if($i % $fizzNum === 0 && $i % $buzzNum === 0 ){
                     echo 'FizzBuzz '.$fizzNum;  //表示
                     echo "</br>";               //改行
                     $fizzNum += $num1;          //倍数計算
                     $buzzNum += $num2;          //倍数計算
                 }
 
-                //Fizz表紙
-                else if($i == $fizzNum){
+                //Fizz表示
+                else if($i % $fizzNum === 0 ){
                     echo 'Fizz '.$fizzNum;  //表示
                     echo "</br>";           //改行
                     $fizzNum += $num1;      //倍数計算
                 }
 
                 //Buzz表示
-                else if($i == $buzzNum){
+                else if($i % $buzzNum === 0 ){
                     echo 'Buzz '.$buzzNum;  //表示
                     echo "</br>";           //改行
                     $buzzNum += $num2;      //倍数計算
                 }
-
             }
+        }
         ?>
-    </p>
+    </p> 
 </body>
 </html>
